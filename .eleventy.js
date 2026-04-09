51b887fcdee57da7c883352ea08663e3ec799953
Ahmet Başlık <onlyleofibonacci@gmail.com>
2026-04-09 20:19:41 +0300

    yenilogo3

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,7 @@
 /** @param {import("@11ty/eleventy").UserConfig} eleventyConfig */
 module.exports = function (eleventyConfig) {
   // Varlıklar temiz URL ile kökte: /logo.png, /images/*
-  eleventyConfig.addPassthroughCopy({ "public/logo.png": "logo.png" });
+  eleventyConfig.addPassthroughCopy({ "public/logonew.png": "logonew.png" });
   eleventyConfig.addPassthroughCopy({ "public/images": "images" });
 
   return {
